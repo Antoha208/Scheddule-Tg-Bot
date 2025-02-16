031d5064c24eb448f9d82fae0d7465c7106984c7
Anton Androsiuk <67116318+Antoha208@users.noreply.github.com>
2025-02-16 15:42:52 +0300

    start new

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
+require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
 const schedule = require('node-schedule');
 const messages = require('./messages.json'); // Файл с текстами сообщений
-require('dotenv').config()
 
-
-const TOKEN = process.env.TOKEN
+const TOKEN = process.env.TOKEN;
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 let userStates = {}; // Хранит состояние пользователей в цепочке сообщений
+let scheduledJobs = {}; // Хранит отложенные отправки сообщений
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     userStates[chatId] = 0; // Начинаем с первого сообщения
-    bot.sendMessage(chatId, messages[0], getNextButton());
+    sendMessageWithDelay(chatId);
 });
 
 bot.on('callback_query', (query) => {
@@ -20,12 +20,35 @@ bot.on('callback_query', (query) => {
     if (query.data === 'next') {
         userStates[chatId] = (userStates[chatId] || 0) + 1;
         if (userStates[chatId] < messages.length) {
-            bot.sendMessage(chatId, messages[userStates[chatId]], getNextButton());
+            sendMessageWithDelay(chatId);
         } else {
             bot.sendMessage(chatId, 'Это было последнее сообщение.');
+            clearScheduledMessage(chatId);
         }
     }
 });
+
+function sendMessageWithDelay(chatId) {
+    bot.sendMessage(chatId, messages[userStates[chatId]], getNextButton());
+    clearScheduledMessage(chatId);
+    scheduledJobs[chatId] = schedule.scheduleJob(new Date(Date.now() + 24 * 60 * 60 * 1000), () => {
+        userStates[chatId] = (userStates[chatId] || 0) + 1;
+        if (userStates[chatId] < messages.length) {
+            bot.sendMessage(chatId, messages[userStates[chatId]], getNextButton());
+            sendMessageWithDelay(chatId);
+        } else {
+            bot.sendMessage(chatId, 'Это было последнее сообщение.');
+            clearScheduledMessage(chatId);
+        }
+    });
+}
+
+function clearScheduledMessage(chatId) {
+    if (scheduledJobs[chatId]) {
+        scheduledJobs[chatId].cancel();
+        delete scheduledJobs[chatId];
+    }
+}
 
 function getNextButton() {
     return {
